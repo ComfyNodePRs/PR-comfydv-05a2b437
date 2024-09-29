86f7dc7b6fee7b526e941f1a8650315cbc0a7fa7
darth-veitcher <1722315+darth-veitcher@users.noreply.github.com>
2024-09-29 09:28:32 +0100

    update install

--- install.py
+++ install.py
@@ -11,4 +11,4 @@ if comfy_path is None:
     print(f"\n[bold yellow]WARN: The `COMFYUI_PATH` environment variable is not set. Assuming `{os.path.dirname(__file__)}/../../` as the ComfyUI path.[/bold yellow]", file=sys.stderr)
     comfy_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-subprocess.check_output(executable=sys.executable, args=['-m', 'pip', 'install', '.'])
+subprocess.check_output(executable=sys.executable, args=['python', '-m', 'pip', 'install', '.'])
